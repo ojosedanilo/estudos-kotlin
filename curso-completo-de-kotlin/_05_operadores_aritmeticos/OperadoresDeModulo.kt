7c9fd61bcda9a80c3dcfb29212b7dd9198b426ae
José Danilo <77727610+ojosedanilo@users.noreply.github.com>
2025-12-25 16:01:50 -0300

    Operadores de Módulo

---
+++ curso-completo-de-kotlin/_05_operadores_aritmeticos/OperadoresDeModulo.kt
@@ -0,0 +1,34 @@
+package _05_operadores_aritmeticos
+
+fun main() {
+    // --- Operadores de Módulo ---
+    // %
+    // rem()
+    // mod()
+
+    var n1 = 10
+    // Se o resto for 0, então é par
+    if (n1 % 2 == 0) {
+        println("$n1 é par")
+    } else {
+        println("$n1 é ímpar")
+    }
+
+    println()
+
+    println("10 % 3 = ${10 % 3}")
+    println("10.rem(3) = ${10.rem(3)}")
+    println("10.mod(3) = ${10.mod(3)}")
+
+    println()
+
+    println("-10 % 3 = ${-10 % 3}")
+    println("(-10).rem(3) = ${(-10).rem(3)}")
+    println("(-10).mod(3) = ${(-10).mod(3)}")
+
+    println()
+
+    println("10 % -3 = ${10 % -3}")
+    println("10.rem(-3) = ${10.rem(-3)}")
+    println("10.mod(-3) = ${10.mod(-3)}")
+}
