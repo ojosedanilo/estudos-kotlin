21417ce953e4b3d8788f9eb9d4587cef512a85bc
José Danilo <77727610+ojosedanilo@users.noreply.github.com>
2025-12-25 15:44:06 -0300

    Operadores Aritméticos com Números e Strings

---
+++ curso-completo-de-kotlin/_05_operadores_aritmeticos/AritmeticaComNumeros.kt
@@ -0,0 +1,19 @@
+package _05_operadores_aritmeticos
+
+fun main() {
+    var n1 = 10
+    var n2 = 20
+    var n3 = 10.0f
+
+    var soma = n1 + n2
+    var subtracao = n1 - n2
+    var multiplicacao = n1 * n2
+    var divisaoInteira = n1 / n2
+    var divisao: Float = n3 / n2
+
+    println(soma)
+    println(subtracao)
+    println(multiplicacao)
+    println(divisaoInteira)
+    println(divisao)
+}
---
+++ curso-completo-de-kotlin/_05_operadores_aritmeticos/AritmeticaComStrings.kt
@@ -0,0 +1,22 @@
+package _05_operadores_aritmeticos
+
+fun main() {
+    var n1 = "Carro"
+    var n2 = 20
+
+    // Quando o primeiro elemento de uma adição é uma
+    // String, assume-se que o resultado é uma String
+    // e então concatena (junta) os operandos
+    var resultado = n1 + n2
+    println(resultado)
+
+    // Mas a linha abaixo dá erro
+    // var resultado2 = n2 + n1
+    // Uma solução seria começar com uma String vazia
+    var resultado2 = "" + n2 + n1
+    println(resultado2)
+
+    // Também poderia converter n2 para uma string
+    resultado2 = n2.toString() + n1
+    println(resultado2)
+}
